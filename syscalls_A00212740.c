60809ca9eecaecc06bfc46c70187f471fa8c1490
Christian <crisdavcar@gmail.com>
2017-03-14 11:28:53 -0500

    syscall process timer

--- syscalls_A00212740.c
+++ syscalls_A00212740.c
@@ -1,1 +1,23 @@
+#define DIVISOR 1000000000L
+
+#include<stdio.h>
+#include<time.h>
+
+void main(void)
+{
+time_t tiempo;
+struct timespec inicio, fin;
+
+
+clock_gettime(CLOCK_REALTIME, &inicio);
+
+sleep(5);
+
+clock_gettime(CLOCK_REALTIME, &fin);
+
+double total = ( fin.tv_sec - inicio.tv_sec ) + ( fin.tv_nsec - inicio.tv_nsec) / DIVISOR;
+
+printf("El tiempo que transcurrio en segundos fue: %1f\n", total);
+
+}
 
